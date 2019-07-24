d762b8a4facd0bbe8e03c81f4f25b2beb5108c20
shiddukuri2275 <shiddukuri2275@gmail.com>
2019-07-24 12:57:20 +0530

    new changes applied

--- src/main/java/com/padma/apps/AppLock.java
+++ src/main/java/com/padma/apps/AppLock.java
@@ -4,6 +4,7 @@ public class AppLock {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// slight changes added
       System.out.println("App lock at an interval...");
 	}
 
